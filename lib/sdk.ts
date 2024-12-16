2f4af4a1ff1450eda1a6de4dcbbc6b6f4f125eda
Aron Braun <abraunu@gmail.com>
2024-12-15 19:25:41 -0500

    refactor: cleanup and simplify code

--- lib/sdk.ts
+++ lib/sdk.ts
@@ -6,72 +6,187 @@ import {
 } from "./utils";
 import { validateConfig } from "./validator";
 
-let simpleConfig: Partial<SimpleConfig> = {};
+const state = {
+  config: {} as Partial<SimpleConfig>,
+  abortControllers: {
+    emailListener: createRenewableAbortController(),
+    emailCheck: createRenewableAbortController(),
+  },
+  emailValidation: {
+    lastResult: {
+      email: "",
+      isValid: false,
+    },
+  },
+  autoOpenTimeout: undefined as Timer | undefined,
+};
 
+// Event listener for postMessage
 window.addEventListener("message", (event: MessageEvent) => {
-  if (event.origin !== getBaseUrl()) return;
-
-  if (event.data.type === "success") simpleConfig.onSuccess?.(event.data);
+  if (event.origin === getBaseUrl() && event.data.type === "success") {
+    state.config.onSuccess?.(event.data);
+  }
 });
 
+// Main configuration function
 window.applySimpleConfig = function (config) {
   if (config) {
-    simpleConfig = { ...simpleConfig, ...config };
-    validateConfig(simpleConfig)
-      .then((errors) => {
-        injectSimpleIcon(errors?.length === 0);
-      })
+    state.config = { ...state.config, ...config };
+    validateConfig(state.config)
+      .then((errors) => applySimple(!errors?.length))
       .catch((error) => {
         console.error("Failed to validate config:", error);
-        injectSimpleIcon(false);
+        applySimple(false);
       });
   } else {
     console.debug("Simple config: Clearing all config...");
-    simpleConfig = {};
-    injectSimpleIcon(false);
+    state.config = {};
+    applySimple(false);
   }
-  console.debug("Simple config:", simpleConfig);
+  console.debug("Simple config:", state.config);
 };
 
-const inputListenerAbortController = createRenewableAbortController();
-const checkEmailAbortController = createRenewableAbortController();
-let openPopupOnEmailChangeTimeout: Timer | undefined;
-const lastEmailValidationResult = {
-  email: "",
-  isValid: false,
-};
+function openPaymentPopup() {
+  const width = 500;
+  const height = 600;
+  const left = (window.screen.width - width) / 2 + window.screenLeft;
+  const top = (window.screen.height - height) / 2;
 
-function injectSimpleIcon(inject: boolean) {
-  //cleanup old listeners
-  clearTimeout(openPopupOnEmailChangeTimeout);
-  checkEmailAbortController.renew();
-  if (!inject) inputListenerAbortController.renew();
+  const params = {
+    platformId: state.config.platformId,
+    organizationTaxId: state.config.organizationTaxId,
+    amount: state.config.amount,
+    email: state.config.email,
+    ...state.config.schedule,
+  };
 
-  // auto open popup if email is set and valid
+  window.open(
+    `${getBaseUrl()}/payment?${new URLSearchParams(removeEmptyValues(params)).toString()}`,
+    "PopupWindow",
+    `width=${width},height=${height},left=${left},top=${top}`,
+  );
+}
+
+function addSimpleIcon(input: HTMLInputElement) {
+  console.debug("Injecting Simple icon");
+
+  const wrapper = document.createElement("div");
+  wrapper.classList.add("simple-wrapper");
+  wrapper.style.cssText = "position: relative; display: inherit; width: 100%;";
+
+  input.parentNode?.insertBefore(wrapper, input);
+  wrapper.appendChild(input);
+  input.style.paddingRight = "40px";
+
+  const icon = document.createElement("div");
+  icon.innerHTML = PAY_WITH_SIMPLE_SVG;
+  icon.style.cssText = `
+    position: absolute;
+    right: 10px;
+    top: 50%;
+    transform: translateY(-50%);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+    opacity: 0;
+    transition: opacity 0.5s ease;
+  `;
+  icon.title = "Pay with Simple";
+  wrapper.appendChild(icon);
+
+  // Trigger reflow and fade in the icon
+  setTimeout(() => (icon.style.opacity = "1"), 0);
+
+  icon.addEventListener("click", openPaymentPopup);
+  input.addEventListener(
+    "change",
+    () => window.applySimpleConfig({ email: input.value }),
+    { signal: state.abortControllers.emailListener.signal },
+  );
+
+  input.dataset.simpleIconAdded = "true";
+  console.debug("Simple icon added");
+}
+
+function removeSimpleIcon(input: HTMLInputElement) {
+  console.info("Removing Simple icon");
+  const wrapper = input.parentElement;
+  if (wrapper?.classList.contains("simple-wrapper")) {
+    const icon: HTMLDivElement | null = wrapper.querySelector("div:last-child");
+    // Fade out the icon
+    if (icon) icon.style.opacity = "0";
+
+    setTimeout(() => {
+      // Move the input back to its original position
+      wrapper.parentNode?.insertBefore(input, wrapper);
+      // Remove the wrapper (which also removes the icon)
+      wrapper.remove();
+    }, 500);
+  }
+
+  // Reset the input styles
+  input.style.paddingRight = "";
+  input.dataset.simpleIconAdded = "false";
+}
+
+// Helper function to validate email and open popup
+async function checkEmailAndOpenPopup() {
+  const { email } = state.config;
+  const { lastResult } = state.emailValidation;
+
+  // Validate email format
   if (
-    simpleConfig.email &&
-    simpleConfig.email.match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/)
-  ) {
-    if (lastEmailValidationResult.email === simpleConfig.email) {
-      if (lastEmailValidationResult.isValid)
-        openPopupOnEmailChangeTimeout = setTimeout(openPaymentPopup, 1000);
-    } else {
-      openPopupOnEmailChangeTimeout = setTimeout(async () => {
-        const response = await fetch(
-          `${getBaseUrl()}/api/popup/check-email?platformId=${simpleConfig.platformId}&email=${simpleConfig.email}`,
-          { signal: checkEmailAbortController.signal },
-        );
-        if (response.ok) {
-          const isRegistered = (await response.json()).isRegistered;
-          if (isRegistered) openPaymentPopup();
-          lastEmailValidationResult.email = simpleConfig.email || "";
-          lastEmailValidationResult.isValid = isRegistered;
-          if (isRegistered) openPaymentPopup();
-        }
-      }, 1000);
+    !email?.match(
+      /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i,
+    )
+  )
+    return;
+
+  // Use cached result if available
+  if (lastResult.email === email) {
+    if (lastResult.isValid) {
+      console.debug("Using cached email validation result");
+      openPaymentPopup();
+    }
+    return;
+  }
+
+  // If not cached, check if email is registered
+  try {
+    console.debug("Checking if email is registered:", email);
+    const response = await fetch(
+      `${getBaseUrl()}/api/popup/check-email?platformId=${state.config.platformId}&email=${email}`,
+      { signal: state.abortControllers.emailCheck.signal },
+    );
+
+    if (response.ok) {
+      const { isRegistered } = await response.json();
+      lastResult.email = email;
+      lastResult.isValid = isRegistered;
+
+      if (isRegistered) {
+        console.debug("Email is registered, opening popup");
+        openPaymentPopup();
+      }
+    }
+  } catch (error) {
+    if (error instanceof Error && error.name !== "AbortError") {
+      console.error("Failed to validate email:", error);
     }
   }
+}
 
+function applySimple(apply: boolean) {
+  // Cleanup
+  clearTimeout(state.autoOpenTimeout);
+  state.abortControllers.emailCheck.renew();
+  if (!apply) state.abortControllers.emailListener.renew();
+
+  // Auto open popup if email is valid
+  state.autoOpenTimeout = setTimeout(checkEmailAndOpenPopup, 1000);
+
+  // Find and process all email inputs
   let emailInputs = document.querySelectorAll(
     ".simple-email input, input.simple-email",
   ) as NodeListOf<HTMLInputElement>;
@@ -81,100 +196,9 @@ function injectSimpleIcon(inject: boolean) {
     ) as NodeListOf<HTMLInputElement>;
 
   emailInputs.forEach((input) => {
-    if (input.dataset.simpleIconAdded === inject.toString()) return;
+    if (input.dataset.simpleIconAdded === apply.toString()) return;
 
-    if (!inject) {
-      console.info("Removing Simple icon");
-      // Find the wrapper div (parent of the input)
-      const wrapper = input.parentElement;
-      if (wrapper && wrapper.classList.contains("simple-wrapper")) {
-        const icon: HTMLDivElement | null =
-          wrapper.querySelector("div:last-child");
-        // Fade out the icon
-        if (icon) icon.style.opacity = "0";
-        setTimeout(() => {
-          // Move the input back to its original position
-          wrapper.parentNode?.insertBefore(input, wrapper);
-          // Remove the wrapper (which also removes the icon)
-          wrapper.remove();
-        }, 500);
-      }
-
-      // Reset the input styles
-      input.style.paddingRight = "";
-
-      input.dataset.simpleIconAdded = "false";
-      return;
-    }
-
-    console.debug("Injecting Simple icon");
-
-    const wrapper = document.createElement("div");
-    wrapper.classList.add("simple-wrapper");
-
-    wrapper.style.position = "relative";
-    wrapper.style.display = "inherit";
-    wrapper.style.width = "100%";
-
-    input.parentNode?.insertBefore(wrapper, input);
-    wrapper.appendChild(input);
-
-    input.style.paddingRight = "40px";
-
-    const icon = document.createElement("div");
-    icon.innerHTML = PAY_WITH_SIMPLE_SVG;
-    icon.style.position = "absolute";
-    icon.style.right = "10px";
-    icon.style.top = "50%";
-    icon.style.transform = "translateY(-50%)";
-    icon.style.display = "flex";
-    icon.style.alignItems = "center";
-    icon.style.justifyContent = "center";
-    icon.style.cursor = "pointer";
-    icon.style.opacity = "0"; // Start with opacity 0
-    icon.style.transition = "opacity 0.5s ease"; // Add transition
-    icon.title = "Pay with Simple";
-
-    wrapper.appendChild(icon);
-
-    // Trigger reflow and fade in the icon
-    setTimeout(() => {
-      icon.style.opacity = "1";
-    }, 0);
-
-    icon.addEventListener("click", openPaymentPopup);
-
-    input.addEventListener(
-      "change",
-      () => window.applySimpleConfig({ email: input.value }),
-      { signal: inputListenerAbortController.signal },
-    );
-
-    input.dataset.simpleIconAdded = "true";
-    console.debug("Simple icon added");
+    if (apply) addSimpleIcon(input);
+    else removeSimpleIcon(input);
   });
-}
-
-function openPaymentPopup() {
-  const width = 500;
-  const height = 600;
-
-  const left = (window.screen.width - width) / 2 + window.screenLeft;
-  const top = (window.screen.height - height) / 2;
-
-  const params = {
-    platformId: simpleConfig.platformId,
-    organizationTaxId: simpleConfig.organizationTaxId,
-    amount: simpleConfig.amount,
-    email: simpleConfig.email,
-    ...simpleConfig.schedule,
-  };
-
-  window.open(
-    `${getBaseUrl()}/payment?${new URLSearchParams(
-      removeEmptyValues(params),
-    ).toString()}`,
-    "PopupWindow",
-    `width=${width},height=${height},left=${left},top=${top}`,
-  );
 }
