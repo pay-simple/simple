be816b549491da3ea79e29626e8884a532db2f40
rajput-hemant <rajput.hemant2001@gmail.com>
2024-12-20 20:18:17 +0530

    feat: add functionality to publish types
    Signed-off-by: rajput-hemant <rajput.hemant2001@gmail.com>

--- scripts/publish-cli.ts
+++ scripts/publish-cli.ts
@@ -8,14 +8,25 @@ import semver from "semver";
 
 import { parseErrorMessage } from "../lib/utils";
 
+type PackageName = `@paysimple/simple${"" | "-dev" | "-types" | "-dev-types"}`;
+
 const execPromise = util.promisify(exec);
+
+let latestVersion: Record<PackageName, string | null> = {
+  "@paysimple/simple": null,
+  "@paysimple/simple-dev": null,
+  "@paysimple/simple-types": null,
+  "@paysimple/simple-dev-types": null,
+};
 
 /**
  * Fetches the latest version of a given package from the npm registry.
- * @param {string} packageName - The name of the package to fetch the version for.
+ * @param {PackageName} packageName - The name of the package to fetch the version for.
  * @returns {Promise<string>} - The latest version of the package.
  */
-const getLatestVersion = async (packageName: string): Promise<string> => {
+const fetchLatestVersion = async (
+  packageName: PackageName,
+): Promise<string> => {
   const response = await fetch(`https://registry.npmjs.org/${packageName}`);
   const data = await response.json();
 
@@ -30,6 +41,15 @@ const getLatestVersion = async (packageName: string): Promise<string> => {
 };
 
 /**
+ * Retrieves the latest version of a given package.
+ * @param {PackageName} packageName - The name of the package to retrieve the version for.
+ * @returns {string | null} - The latest version of the package or null if not found.
+ */
+function getLatestVersion(packageName: PackageName) {
+  return latestVersion[packageName];
+}
+
+/**
  * Runs a series of prompts to gather user input for publishing a package.
  * @returns {Promise<object>} - The responses from the prompts.
  */
@@ -40,10 +60,11 @@ const runPrompts = async (): Promise<
     | "customVersion"
     | "verifyVersion"
     | "confirmPublish"
+    | "typesVersionType"
+    | "typesCustomVersion"
+    | "typesVerifyVersion"
   >
 > => {
-  let latestVersion: string | null = null;
-
   const responses = await prompts(
     [
       {
@@ -60,9 +81,6 @@ const runPrompts = async (): Promise<
             value: "@paysimple/simple",
           },
         ],
-        onState: async (state) => {
-          latestVersion = await getLatestVersion(state.value);
-        },
       },
       {
         type: "select",
@@ -79,16 +97,70 @@ const runPrompts = async (): Promise<
         type: (prev) => (prev === "custom" ? "text" : null),
         name: "customVersion",
         message: "Enter the custom version:",
-        initial: latestVersion ? latestVersion : undefined,
+        initial: (_, { environment }) => getLatestVersion(environment) ?? "",
       },
       {
         type: "text",
         name: "verifyVersion",
         message: "Enter the version to verify:",
-        initial: (_, values) =>
-          values.versionType === "custom"
-            ? values.customVersion
-            : semver.inc(latestVersion!, values.versionType),
+        initial: (_, values) => {
+          if (values.versionType === "custom") {
+            return values.customVersion;
+          }
+
+          return semver.inc(
+            getLatestVersion(values.environment) ?? "",
+            values.versionType,
+          );
+        },
+        validate: (value) => {
+          if (!semver.valid(value)) {
+            return "Invalid version";
+          }
+          return true;
+        },
+      },
+      {
+        type: "select",
+        name: "typesVersionType",
+        message: "Select the type of version release for types:",
+        choices: [
+          { title: "Major", value: "major" },
+          { title: "Minor", value: "minor" },
+          { title: "Patch", value: "patch" },
+          { title: "Define version yourself", value: "custom" },
+        ],
+      },
+      {
+        type: (prev) => (prev === "custom" ? "text" : null),
+        name: "typesCustomVersion",
+        message: "Enter the custom version for types:",
+        initial: (_, { environment }) => {
+          const packageName = environment.includes("dev")
+            ? "@paysimple/simple-dev-types"
+            : "@paysimple/simple-types";
+
+          return getLatestVersion(packageName) ?? "";
+        },
+      },
+      {
+        type: "text",
+        name: "typesVerifyVersion",
+        message: "Enter the version to verify for types:",
+        initial: (_, values) => {
+          if (values.typesVersionType === "custom") {
+            return values.typesCustomVersion;
+          }
+
+          const packageName = values.environment.includes("dev")
+            ? "@paysimple/simple-dev-types"
+            : "@paysimple/simple-types";
+
+          return semver.inc(
+            getLatestVersion(packageName) ?? "",
+            values.typesVersionType,
+          );
+        },
         validate: (value) => {
           if (!semver.valid(value)) {
             return "Invalid version";
@@ -99,7 +171,7 @@ const runPrompts = async (): Promise<
       {
         type: "confirm",
         name: "confirmPublish",
-        message: "Are you sure you want to publish?",
+        message: "Are you sure you want to publish both the package and types?",
         initial: true,
       },
     ],
@@ -135,7 +207,7 @@ const ensureDistPathExists = (distPath: string) => {
 const updatePackageJson = (
   rootPath: string,
   distPath: string,
-  packageName: string,
+  packageName: PackageName,
   newVersion: string,
 ) => {
   const packageJsonPath = path.join(rootPath, "package.npm.json");
@@ -143,6 +215,12 @@ const updatePackageJson = (
 
   packageJson.name = packageName;
   packageJson.version = newVersion;
+
+  if (packageName.includes("types")) {
+    packageJson.main = "index.d.ts";
+    packageJson.types = "index.d.ts";
+    packageJson.files = ["index.d.ts"];
+  }
 
   fs.writeFileSync(
     path.join(distPath, "package.json"),
@@ -190,26 +268,80 @@ const updateReadmeUrls = (
 
 /**
  * Prepares and publishes the package to npm.
- * @param {string} packageName - The name of the package to publish.
+ * @param {PackageName} packageName - The name of the package to publish.
  * @param {string} newVersion - The version of the package to publish.
  */
-const publishPackage = async (packageName: string, newVersion: string) => {
+const publishPackage = async (
+  packageName: PackageName,
+  newVersion: string,
+  newTypesVersion: string,
+) => {
   const rootPath = path.resolve(__dirname, "..");
-  const distPath = path.resolve(__dirname, "../dist");
+  const distPath = path.resolve(
+    __dirname,
+    "../dist",
+    packageName.includes("dev") ? "dev" : "prod",
+  );
+  const typesDistPath = path.resolve(
+    __dirname,
+    "../dist",
+    packageName.includes("dev") ? "dev-types" : "types",
+  );
+
   try {
     ensureDistPathExists(distPath);
+    ensureDistPathExists(typesDistPath);
     console.log(colors.cyan("Preparing files for publishing..."));
+
+    // Update and copy package.json
     updatePackageJson(rootPath, distPath, packageName, newVersion);
+    updatePackageJson(
+      rootPath,
+      typesDistPath,
+      packageName.includes("dev")
+        ? "@paysimple/simple-dev-types"
+        : "@paysimple/simple-types",
+      newTypesVersion,
+    );
+
+    // Update and copy README.md
     updateReadmeUrls(rootPath, distPath, packageName, newVersion);
-    // copy types
+
+    // Copy README.md to dist and types dist
     fs.copyFileSync(
-      path.join(rootPath, "types", "globals.d.ts"),
-      path.join(distPath, "index.d.ts"),
+      path.join(rootPath, "README.npm.md"),
+      path.join(distPath, "README.md"),
+    );
+    fs.copyFileSync(
+      path.join(rootPath, "README.npm.md"),
+      path.join(typesDistPath, "README.md"),
     );
 
     console.log(
-      colors.yellow(`Building ${packageName} with version ${newVersion}...`),
+      colors.yellow(
+        `\nBuilding ${packageName} with version ${newVersion}...\n`,
+      ),
     );
+
+    // Ensure the build step is active
+    await execPromise(`bun run build`);
+
+    // Check if index.js exists before copying
+    const indexPath = path.join(rootPath, "dist", "index.js");
+    if (fs.existsSync(indexPath)) {
+      fs.copyFileSync(indexPath, path.join(distPath, "index.js"));
+    } else {
+      throw new Error(`index.js not found at ${indexPath}`);
+    }
+
+    // Check if globals.d.ts exists before copying
+    const typesPath = path.join(rootPath, "types", "globals.d.ts");
+    if (fs.existsSync(typesPath)) {
+      fs.copyFileSync(typesPath, path.join(distPath, "index.d.ts"));
+      fs.copyFileSync(typesPath, path.join(typesDistPath, "index.d.ts"));
+    } else {
+      throw new Error(`globals.d.ts not found at ${typesPath}`);
+    }
 
     // Check if we're publishing to production
     const isProduction = packageName === "@paysimple/simple";
@@ -218,19 +350,30 @@ const publishPackage = async (packageName: string, newVersion: string) => {
       process.env.NODE_ENV = "production";
     }
 
-    await execPromise(`bun run build`);
-
     if (isProduction) {
-      console.log(
-        colors.yellow("\nProduction package prepared for publishing!"),
-      );
+      console.log(colors.yellow("Production package prepared for publishing!"));
       console.log(colors.cyan("\nPlease verify the following:"));
       console.log("1. Check the contents of the dist folder");
       console.log("2. Verify the package.json version and name");
       console.log("3. Confirm the README CDN URLs are correct");
       console.log("\nThen run manually:");
+
+      console.log(
+        colors.cyan("Run the following command to publish the main package:"),
+      );
       console.log(
         colors.green(`npm publish ${distPath} --access public --tag latest`),
+      );
+
+      console.log(
+        colors.cyan(
+          "\nRun the following command to publish the types package:",
+        ),
+      );
+      console.log(
+        colors.green(
+          `npm publish ${typesDistPath} --access public --tag latest`,
+        ),
       );
     } else {
       console.log(
@@ -241,7 +384,21 @@ const publishPackage = async (packageName: string, newVersion: string) => {
       await execPromise(`npm publish ${distPath} --access public --tag latest`);
       console.log(
         colors.green(
-          `Successfully published ${packageName} version ${newVersion}!`,
+          `Successfully published ${packageName} with version ${newVersion}!\n`,
+        ),
+      );
+
+      console.log(
+        colors.yellow(
+          `Publishing types to ${packageName} with version ${newTypesVersion}...`,
+        ),
+      );
+      await execPromise(
+        `npm publish ${typesDistPath} --access public --tag latest`,
+      );
+      console.log(
+        colors.green(
+          `Successfully published types for ${packageName} with version ${newTypesVersion}!\n`,
         ),
       );
     }
@@ -256,15 +413,41 @@ const publishPackage = async (packageName: string, newVersion: string) => {
 const init = async () => {
   console.clear();
   console.log(`${colors.bgGreenBright(colors.bold("Simple Publish CLI"))}\n`);
+  console.log(colors.cyan("Fetching latest versions..."));
+
+  const [
+    latestProdVersion,
+    latestDevVersion,
+    latestProdTypesVersion,
+    latestDevTypesVersion,
+  ] = await Promise.all([
+    fetchLatestVersion("@paysimple/simple"),
+    fetchLatestVersion("@paysimple/simple-dev"),
+    fetchLatestVersion("@paysimple/simple-types"),
+    fetchLatestVersion("@paysimple/simple-dev-types"),
+  ]);
+
+  console.log(colors.green("Latest versions fetched"));
+
+  latestVersion = {
+    "@paysimple/simple-dev": latestDevVersion,
+    "@paysimple/simple-dev-types": latestDevTypesVersion,
+    "@paysimple/simple": latestProdVersion,
+    "@paysimple/simple-types": latestProdTypesVersion,
+  };
+
+  console.log(colors.cyan("\nRunning prompts...\n"));
 
   try {
-    const { verifyVersion, environment, confirmPublish } = await runPrompts();
-    const newVersion = verifyVersion;
+    const { environment, verifyVersion, typesVerifyVersion, confirmPublish } =
+      await runPrompts();
 
+    // Handle main package publishing
     if (confirmPublish) {
-      await publishPackage(environment, newVersion);
+      console.log(colors.cyan("Publishing main package..."));
+      await publishPackage(environment, verifyVersion, typesVerifyVersion);
     } else {
-      console.log(colors.red("Publish cancelled"));
+      console.log(colors.red("Main package publish cancelled"));
     }
   } catch (error) {
     console.error(colors.red(`Failed to publish: ${parseErrorMessage(error)}`));
